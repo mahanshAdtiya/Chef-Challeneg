9004b9c22bd91b4fb657d9251d3a62d8504bff97
0oAVIRALo0 <aviral21029@iiitd.ac.in>
2025-01-13 19:35:01 +0530

    removed clg

--- src/Components/Winner.jsx
+++ src/Components/Winner.jsx
@@ -11,21 +11,23 @@ import {
   LinkedinIcon,
   FacebookIcon,
   TelegramIcon,
-  RedditIcon
+  RedditIcon,
 } from "react-share";
 
-const Winner = ({ word, resetGame, time, message}) => {
-  const shareUrl = window.location.origin;
+const Winner = ({ word, resetGame, time, message }) => {
+  const shareUrl = window.location.href;
   return (
     <div className="winner-container">
       <h1 className="winner-heading">Winner!</h1>
       <p className="winner-content">The word was: {word.word.toUpperCase()}</p>
       <p className="winner-content">{word.clue}</p>
-      <p>Time Left:  <span style={{color: 'teal'}}>{time}</span></p>
+      <p>
+        Time Left: <span style={{ color: "teal" }}>{time}</span>
+      </p>
       <button className="winner-btn" onClick={resetGame}>
         Play Again
       </button>
-      <div style={{display: 'flex', gap: '20px'}}>
+      <div style={{ display: "flex", gap: "20px" }}>
         <WhatsappShareButton url={shareUrl} title={message}>
           <WhatsappIcon size={32} round />
         </WhatsappShareButton>
